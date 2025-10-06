d4d7adbc87562d01c2efdd711f1edf869514165a
Mustafa Haj <Mustafa.Hajmohammad@triosstudent.com>
2025-10-06 12:18:20 -0400

    Initial commit for Scavenger Hunt App

---
+++ ScavengerHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScavengerHunt
+//
+//  Created by Satoof Haj  on 2025-10-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScavengerHunt/ScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntApp.swift
+//  ScavengerHunt
+//
+//  Created by Satoof Haj  on 2025-10-06.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
